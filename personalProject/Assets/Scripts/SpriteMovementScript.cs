30fe2d0bcf2dd8d7a2a7424cbbf1c6f2bb5cd1d8
Donal McClean <donal@accessearth.com>
2020-09-17 23:31:32 +0100

    Bunny hop and movement

---
+++ personalProject/Assets/Scripts/SpriteMovementScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private Rigidbody2D myRigidbody;
+    [SerializeField]
+    private Collider2D myCollider;
+
+    private bool canJump = true;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            myRigidbody.velocity = new Vector2((moveSpeed*-1), myRigidbody.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (canJump)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+                canJump = false;
+            }      
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump = true;
+    }
+}
