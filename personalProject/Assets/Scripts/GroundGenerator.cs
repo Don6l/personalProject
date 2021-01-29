e5aee03e98d9ebd29c849fdf89837f4022b39630
Donal McClean <donal@accessearth.com>
2021-01-29 17:01:17 +0000

    minor update

--- personalProject/Assets/Scripts/GroundGenerator.cs
+++ personalProject/Assets/Scripts/GroundGenerator.cs
@@ -9,7 +9,9 @@ public class GroundGenerator : MonoBehaviour
     [SerializeField]
     private GameObject ground;
     [SerializeField]
-    private Transform generationPoint;
+    private Transform generationPointRight;
+    [SerializeField]
+    private Transform generationPointLeft;
     private float groundCount = 0;
     private float groundWidth;
     // Start is called before the first frame update
@@ -19,12 +21,18 @@ public class GroundGenerator : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        if (transform.position.x < generationPoint.position.x){
+        if(transform.position.x < generationPointRight.position.x){
             groundCount++;
             Debug.Log(groundCount);
             transform.position = new Vector3(transform.position.x + groundWidth, transform.position.y, transform.position.z);
             Instantiate(ground, transform.position, transform.rotation, transform);
         }
+        /*if(transform.position.x > generationPoint.position.x){
+            groundCount--;
+            Debug.Log(groundCount);
+            transform.position = new Vector3(transform.position.x + groundWidth, transform.position.y, transform.position.z);
+            Instantiate(ground, transform.position, transform.rotation, transform);
+        }*/
         //Debug.Log("False");
     }
 }
